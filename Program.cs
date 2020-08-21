cea6679ba63c10b1b4ef9ab6ef5c49acbf25d6b0
Benny <ChelseaStats@users.noreply.github.com>
2020-08-22 00:12:30 +0100

    just testing c# net core 3.1 sdk on mac vscode
    Signed-off-by: Benny <ChelseaStats@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Benny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fizz Buzz test!");
+
+            FizzBuzzer();
+
+            Console.Read();
+        }
+
+        private static void FizzBuzzer()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string output = "";
+                if (i % 15 == 0)
+                {
+                    output = " FizzBuzz";
+                }
+                else if (i % 3 == 0)
+                {
+                    output = " Fizz";
+                }
+                else if (i % 5 == 0)
+                {
+                    output = " Buzz";
+                }
+
+                Console.WriteLine(i + output);
+            }
+        }
+    }
+}
